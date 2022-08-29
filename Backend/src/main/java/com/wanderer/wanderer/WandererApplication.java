3cad0b2fb6bb4ed4cfbbb78568adb037daeb6bc1
Gaurav <gauravsingha32@gmail.com>
2022-08-29 13:15:14 +0530

    Initilized Spring

---
+++ Backend/src/main/java/com/wanderer/wanderer/WandererApplication.java
@@ -0,0 +1,13 @@
+package com.wanderer.wanderer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WandererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WandererApplication.class, args);
+	}
+
+}
